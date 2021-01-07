87df9d23d72f85e4be765bed9d6066f7d096cb05
Pupan Phonkaew <43066870+Pupan-cpe@users.noreply.github.com>
2021-01-07 16:24:24 +0700

    test

--- beacon.php
+++ beacon.php
@@ -1,18 +1,18 @@
 <?php
 
 
-$API_URL = 'https://api.line.me/v2/bot/message';
+$API_URL = 'https=>//api.line.me/v2/bot/message';
 $ACCESS_TOKEN = 'nbUIY4GcXs7w7ffWYv/L0qRgK64ijNNsOV1RvCabrmFCSCmEzBi2UCS8Tv2nmWUiVmFV1YjtAqdWQAVGSFK4/v7IaPIwRMOVNYR+SlJ2H13hsoMbjWftBp/rBkgM3BrNuHbIsTZKT/Lv851QH3i6cgdB04t89/1O/w1cDnyilFU='; 
 $channelSecret = '42ca5cec5f0f16736ca482d18b16d55a';
 
 
-$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
+$POST_HEADER = array('Content-Type=> application/json', 'Authorization=> Bearer ' . $ACCESS_TOKEN);
 
-$request = file_get_contents('php://input');   // Get request content
+$request = file_get_contents('php=>//input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
 var_export($request_array);
 
-$url="http://api.openweathermap.org/data/2.5/forecast?q=Albany,usl&APPID=5099c5feb579c7a17b030de0d009282f&units=metric";
+$url="http=>//api.openweathermap.org/data/2.5/forecast?q=Albany,usl&APPID=5099c5feb579c7a17b030de0d009282f&units=metric";
 $json=file_get_contents($url);
 $data=json_decode($json);
 
@@ -20,10 +20,10 @@ echo '<h1>', $data->city->name, ' (', $data->city->country, ')</h1>';
 
 
 // the general information about the weather
- echo '<h2>Temperature:</h2>';
-echo '<p><strong>Current:</strong> ', $data1 =  $data->list[0]->main->temp, '&deg; C</p>';
-echo '<p><strong>Min:</strong> ', $data2 =  $data->list[0]->main->temp_min, '&deg; C</p>';
-echo '<p><strong>Max:</strong> ', $data3 =   $data->list[0]->main->temp_max, '&deg; C</p>';
+ echo '<h2>Temperature=></h2>';
+echo '<p><strong>Current=></strong> ', $data1 =  $data->list[0]->main->temp, '&deg; C</p>';
+echo '<p><strong>Min=></strong> ', $data2 =  $data->list[0]->main->temp_min, '&deg; C</p>';
+echo '<p><strong>Max=></strong> ', $data3 =   $data->list[0]->main->temp_max, '&deg; C</p>';
 
 
 
@@ -53,92 +53,33 @@ $jsonFlex = [
       "contents" => [
         [
           "type" => "text",
-          "text" => "Temperature  IN Albany (US)",
+          "text" => "Temperature IN Albany (US)",
           "size" => "lg",
           "align" => "start",
           "weight" => "bold",
           "color" => "#009813"
         ],
-        [
-          "type" => "text",
-          "text" => "$data1 "+" &deg; C",
-          "size" => "3xl",
-          "weight" => "bold",
-          "color" => "#000000"
+        "hero" => [
+          "type"=> "image",
+          "url"=> "https=>//static.bhphotovideo.com/explora/sites/default/files/styles/top_shot/public/Color-Temperature.jpg?itok=yHYqoXAf",
+          "size"=> "full",
+          "aspectRatio"=> "1.51=>1",
+          "aspectMode"=> "fit"
         ],
-        [
-          "type" => "text",
-          "text" => "Rabbit Line Pay",
-          "size" => "lg",
-          "weight" => "bold",
-          "color" => "#000000"
-        ],
-        [
-          "type" => "text",
-          "text" => "2019.02.14 21:47 (GMT+0700)",
-          "size" => "xs",
-          "color" => "#B2B2B2"
-        ],
-        [
-          "type" => "text",
-          "text" => "Payment complete.",
-          "margin" => "lg",
-          "size" => "lg",
-          "color" => "#000000"
-        ]
       ]
     ],
-    "body" => [
-      "type" => "box",
-      "layout" => "vertical",
-      "contents" => [
+    "body"=> [
+      "type"=> "box",
+      "layout"=> "vertical",
+      "contents"=> [
         [
-          "type" => "separator",
-          "color" => "#C3C3C3"
-        ],
-        [
-          "type" => "box",
-          "layout" => "baseline",
-          "margin" => "lg",
-          "contents" => [
-            [
-              "type" => "text",
-              "text" => "Temp",
-              "align" => "start",
-              "color" => "#C3C3C3"
-            ],
-            [
-              "type" => "text",
-              "text" => "$data1",
-              "align" => "end",
-              "color" => "#000000"
-            ]
-          ]
-        ],
-        [
-          "type" => "box",
-          "layout" => "baseline",
-          "margin" => "lg",
-          "contents" => [
-            [
-              "type" => "text",
-              "text" => "New balance",
-              "color" => "#C3C3C3"
-            ],
-            [
-              "type" => "text",
-              "text" => "฿ 45.57",
-              "align" => "end"
-            ]
-          ]
-        ],
-        [
-          "type" => "separator",
-          "margin" => "lg",
-          "color" => "#C3C3C3"
+          "type"=> "text",
+          "text"=> ".",
+          "align"=> "center",
+          "contents"=> []
         ]
       ]
-    ],
+      ],
     "footer" => [
       "type" => "box",
       "layout" => "horizontal",
@@ -180,7 +121,7 @@ if ( sizeof($request_array['events']) > 0 ) {
 
         $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
 
-        echo "Result: ".$send_result."\r\n";
+        echo "Result=> ".$send_result."\r\n";
         
     }
 }
