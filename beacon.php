546b51ceaf68afb8984efc68af8c5dc9a69423a5
Pupan Phonkaew <43066870+Pupan-cpe@users.noreply.github.com>
2021-01-07 18:15:22 +0700

    update token

--- beacon.php
+++ beacon.php
@@ -2,8 +2,8 @@
 
 
 $API_URL = 'https://api.line.me/v2/bot/message';
-$ACCESS_TOKEN = 'nbUIY4GcXs7w7ffWYv/L0qRgK64ijNNsOV1RvCabrmFCSCmEzBi2UCS8Tv2nmWUiVmFV1YjtAqdWQAVGSFK4/v7IaPIwRMOVNYR+SlJ2H13hsoMbjWftBp/rBkgM3BrNuHbIsTZKT/Lv851QH3i6cgdB04t89/1O/w1cDnyilFU='; 
-$channelSecret = '42ca5cec5f0f16736ca482d18b16d55a';
+$ACCESS_TOKEN = '3BpE+W8GilBhsVOQwJxGBFen60jBtgxOzXoNwXZaPyxmoRUDAeYWKhDpWnHOGUJoVmFV1YjtAqdWQAVGSFK4/v7IaPIwRMOVNYR+SlJ2H10Bt0FyErjEZSnGv+/w93AKBSpuWseLDAfLkb8c3uNjgAdB04t89/1O/w1cDnyilFU='; 
+$channelSecret = 'Ua31d1e98d625c4a5ac675fc47ec537e3';
 
 
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
