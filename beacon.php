2f005c34dbf6254c9e8821d43c1b5b9a2c8ea11c
Pupan Phonkaew <43066870+Pupan-cpe@users.noreply.github.com>
2021-01-07 17:53:09 +0700

    1

--- beacon.php
+++ beacon.php
@@ -43,90 +43,121 @@ echo $data1;
 
 
 $jsonFlex = [
-  [
-    "type" => "flex",
-    "altText" => "Hello Flex Message",
-    "contents" => [
-      "type" => "bubble",
-      "direction" => "ltr",
-      "header" => [
-        "type" => "box",
-        "layout" => "vertical",
-        "contents" => [
-          [
-            "type" => "text",
-            "text" => "Purchase",
-            "size" => "lg",
-            "align" => "start",
-            "weight" => "bold",
-            "color" => "#009813"
-          ],
-          [
-            "type" => "text",
-            "text" => "฿ 100.00",
-            "size" => "3xl",
-            "weight" => "bold",
-            "color" => "#000000"
-          ],
-          [
-            "type" => "text",
-            "text" => "Rabbit Line Pay",
-            "size" => "lg",
-            "weight" => "bold",
-            "color" => "#000000"
-          ],
-          [
-            "type" => "text",
-            "text" => "2019.02.14 21:47 (GMT+0700)",
-            "size" => "xs",
-            "color" => "#B2B2B2"
-          ],
-          [
-            "type" => "text",
-            "text" => "Payment complete.",
-            "margin" => "lg",
-            "size" => "lg",
-            "color" => "#000000"
+  "type" => "flex",
+  "altText" => "Hello Flex Message",
+  "contents" => [
+    "type" => "bubble",
+    "direction" => "ltr",
+    "header" => [
+      "type" => "box",
+      "layout" => "vertical",
+      "contents" => [
+        [
+          "type" => "text",
+          "text" => "Purchase",
+          "size" => "lg",
+          "align" => "start",
+          "weight" => "bold",
+          "color" => "#009813"
+        ],
+        [
+          "type" => "text",
+          "text" => "฿ 100.00",
+          "size" => "3xl",
+          "weight" => "bold",
+          "color" => "#000000"
+        ],
+        [
+          "type" => "text",
+          "text" => "Rabbit Line Pay",
+          "size" => "lg",
+          "weight" => "bold",
+          "color" => "#000000"
+        ],
+        [
+          "type" => "text",
+          "text" => "2019.02.14 21:47 (GMT+0700)",
+          "size" => "xs",
+          "color" => "#B2B2B2"
+        ],
+     
+      ]
+    ],
+    "body" => [
+      "type" => "box",
+      "layout" => "vertical",
+      "contents" => [
+        [
+          "type" => "separator",
+          "color" => "#C3C3C3"
+        ],
+        [
+          "type" => "box",
+          "layout" => "baseline",
+          "margin" => "lg",
+          "contents" => [
+            [
+              "type" => "text",
+              "text" => "Merchant",
+              "align" => "start",
+              "color" => "#C3C3C3"
+            ],
+            [
+              "type" => "text",
+              "text" => "BTS 01",
+              "align" => "end",
+              "color" => "#000000"
+            ]
           ]
+        ],
+        [
+          "type" => "box",
+          "layout" => "baseline",
+          "margin" => "lg",
+          "contents" => [
+            [
+              "type" => "text",
+              "text" => "New balance",
+              "color" => "#C3C3C3"
+            ],
+            [
+              "type" => "text",
+              "text" => "฿ 45.57",
+              "align" => "end"
+            ]
+          ]
+        ],
+        [
+          "type" => "separator",
+          "margin" => "lg",
+          "color" => "#C3C3C3"
         ]
       ]
-      
     ],
-    "hero"=> [
-      "type"=> "image",
-      "url"=> "https=>//static.bhphotovideo.com/explora/sites/default/files/styles/top_shot/public/Color-Temperature.jpg?itok=yHYqoXAf",
-      "size"=> "full",
-      "aspectRatio"=> "1.51:1",
-      "aspectMode"=> "fit"
-    ],
-    "body"=> [
-      "type"=> "box",
-      "layout"=> "vertical",
-      "contents"=> [
+    "footer" => [
+      "type" => "box",
+      "layout" => "horizontal",
+      "contents" => [
         [
-          "type"=> "text",
-          "text"=> ".",
-          "align"=> "center",
-          "contents"=> []
-        ]
-      ]
-    ],
-    "footer"=> [
-      "type"=> "box",
-      "layout"=> "horizontal",
-      "contents"=> [
-        [
-          "type"=> "button",
-          "action"=> [
-            "type"=> "uri",
-            "label"=> "PUPAN",
-            "uri"=> "https://linecorp.com"
+          "type" => "text",
+          "text" => "View Details",
+          "size" => "lg",
+          "align" => "start",
+          "color" => "#0084B6",
+          "action" => [
+            "type" => "uri",
+            "label" => "View Details",
+            "uri" => "https://github.com/pupan-cpe"
           ]
         ]
       ]
     ]
   ]
 ];
+
+
+
+
 
 
 
