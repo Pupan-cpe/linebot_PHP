c2a08f009ec610b50e19345d5f279a4e334da631
Pupan Phonkaew <43066870+Pupan-cpe@users.noreply.github.com>
2021-01-07 18:11:27 +0700

    fix

--- beacon.php
+++ beacon.php
@@ -10,155 +10,125 @@ $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' 
 
 $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
-var_export($request_array);
-
-$url="http://api.openweathermap.org/data/2.5/forecast?q=Albany,usl&APPID=5099c5feb579c7a17b030de0d009282f&units=metric";
-$json=file_get_contents($url);
-$data=json_decode($json);
-
-echo '<h1>', $data->city->name, ' (', $data->city->country, ')</h1>';
-
-
-// the general information about the weather
- echo '<h2>Temperature:</h2>';
-echo '<p><strong>Current:</strong> ', $data1 =  $data->list[0]->main->temp, '&deg; C</p>';
-echo '<p><strong>Min:</strong> ', $data2 =  $data->list[0]->main->temp_min, '&deg; C</p>';
-echo '<p><strong>Max:</strong> ', $data3 =   $data->list[0]->main->temp_max, '&deg; C</p>';
-
-
-
-$myObj->name = "$data1";
-$myObj->age = "$data2";
-$myObj->city = "$data3";
-
-
-$myJSON = json_encode($myObj);
-echo $myJSON;
-echo "-------------------";
-$data=json_decode($myJSON);
-echo $data1;
-
-
-
 
 $jsonFlex = [
-  "type" => "flex",
-  "altText" => "Line Beacon Message",
-  "contents" => [
-    "type" => "bubble",
-    "direction" => "ltr",
-    "header" => [
-      "type" => "box",
-      "layout" => "vertical",
-      "contents" => [
-        [
-          "type" => "text",
-          "text" => "Temp in Albany (US)",
-          "size" => "lg",
-          "align" => "start",
-          "weight" => "bold",
-          "color" => "#009813"
-        ],
-        [
-          "type" => "text",
-          "text" => "$data1",
-          "size" => "3xl",
-          "weight" => "bold",
-          "color" => "#000000"
-        ],
-        [
-          "type" => "text",
-          "text" => ".",
-          "size" => "lg",
-          "weight" => "bold",
-          "color" => "#000000"
-        ],
-        [
-          "type" => "text",
-          "text" => ".",
-          "size" => "xs",
-          "color" => "#B2B2B2"
-        ],
-        [
-          "type" => "text",
-          "text" => "API Success!",
-          "margin" => "lg",
-          "size" => "lg",
-          "color" => "#000000"
-        ]
-      ]
-    ],
-    "body" => [
-      "type" => "box",
-      "layout" => "vertical",
-      "contents" => [
-        [
-          "type" => "separator",
-          "color" => "#C3C3C3"
-        ],
-        [
-          "type" => "box",
-          "layout" => "baseline",
-          "margin" => "lg",
-          "contents" => [
-            [
-              "type" => "text",
-              "text" => "Merchant",
-              "align" => "start",
-              "color" => "#C3C3C3"
-            ],
-            [
-              "type" => "text",
-              "text" => "$data1",
-              "align" => "end",
-              "color" => "#000000"
-            ]
+    "type" => "flex",
+    "altText" => "Hello Flex Message",
+    "contents" => [
+      "type" => "bubble",
+      "direction" => "ltr",
+      "header" => [
+        "type" => "box",
+        "layout" => "vertical",
+        "contents" => [
+          [
+            "type" => "text",
+            "text" => "Purchase",
+            "size" => "lg",
+            "align" => "start",
+            "weight" => "bold",
+            "color" => "#009813"
+          ],
+          [
+            "type" => "text",
+            "text" => "฿ 100.00",
+            "size" => "3xl",
+            "weight" => "bold",
+            "color" => "#000000"
+          ],
+          [
+            "type" => "text",
+            "text" => "Rabbit Line Pay",
+            "size" => "lg",
+            "weight" => "bold",
+            "color" => "#000000"
+          ],
+          [
+            "type" => "text",
+            "text" => "2019.02.14 21:47 (GMT+0700)",
+            "size" => "xs",
+            "color" => "#B2B2B2"
+          ],
+          [
+            "type" => "text",
+            "text" => "Payment complete.",
+            "margin" => "lg",
+            "size" => "lg",
+            "color" => "#000000"
           ]
-        ],
-        [
-          "type" => "box",
-          "layout" => "baseline",
-          "margin" => "lg",
-          "contents" => [
-            [
-              "type" => "text",
-              "text" => "New balance",
-              "color" => "#C3C3C3"
-            ],
-            [
-              "type" => "text",
-              "text" => "฿ 45.57",
-              "align" => "end"
-            ]
-          ]
-        ],
-        [
-          "type" => "separator",
-          "margin" => "lg",
-          "color" => "#C3C3C3"
         ]
-      ]
-    ],
-    "footer" => [
-      "type" => "box",
-      "layout" => "horizontal",
-      "contents" => [
-        [
-          "type" => "text",
-          "text" => "View Details",
-          "size" => "lg",
-          "align" => "start",
-          "color" => "#0084B6",
-          "action" => [
-            "type" => "uri",
-            "label" => "View Details",
-            "uri" => "https://github.com/pupan-cpe"
+      ],
+      "body" => [
+        "type" => "box",
+        "layout" => "vertical",
+        "contents" => [
+          [
+            "type" => "separator",
+            "color" => "#C3C3C3"
+          ],
+          [
+            "type" => "box",
+            "layout" => "baseline",
+            "margin" => "lg",
+            "contents" => [
+              [
+                "type" => "text",
+                "text" => "Merchant",
+                "align" => "start",
+                "color" => "#C3C3C3"
+              ],
+              [
+                "type" => "text",
+                "text" => "BTS 01",
+                "align" => "end",
+                "color" => "#000000"
+              ]
+            ]
+          ],
+          [
+            "type" => "box",
+            "layout" => "baseline",
+            "margin" => "lg",
+            "contents" => [
+              [
+                "type" => "text",
+                "text" => "New balance",
+                "color" => "#C3C3C3"
+              ],
+              [
+                "type" => "text",
+                "text" => "฿ 45.57",
+                "align" => "end"
+              ]
+            ]
+          ],
+          [
+            "type" => "separator",
+            "margin" => "lg",
+            "color" => "#C3C3C3"
+          ]
+        ]
+      ],
+      "footer" => [
+        "type" => "box",
+        "layout" => "horizontal",
+        "contents" => [
+          [
+            "type" => "text",
+            "text" => "View Details",
+            "size" => "lg",
+            "align" => "start",
+            "color" => "#0084B6",
+            "action" => [
+              "type" => "uri",
+              "label" => "View Details",
+              "uri" => "https://google.co.th/"
+            ]
           ]
         ]
       ]
     ]
-  ]
-];
+  ];
 
 
 
